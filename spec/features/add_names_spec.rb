c74f730e72b5106e68c0d5a7c5b3cc3bddf8e90e
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-30 17:03:10 +0100

    added web helper and refactored

--- spec/features/add_names_spec.rb
+++ spec/features/add_names_spec.rb
@@ -1,13 +1,6 @@
 feature 'Add names' do
   scenario 'Can add and store names to the database' do
-    visit('/')
-    fill_in 'player_1_name', with: 'Andrew'
-    fill_in 'player_2_name', with: 'Catriona'
-    click_button "Submit"
-
-    # visit('/names')
-
+    sign_in_and_play
     expect(page).to have_content 'Andrew vs. Catriona'
-
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,11 +1,6 @@
 feature 'See hit points' do
   scenario 'Can see player 2 hit points' do
-    visit('/')
-    fill_in 'player_1_name', with: 'Andrew'
-    fill_in 'player_2_name', with: 'Catriona'
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_content 'Player 2: 100hp'
-
   end
 end
